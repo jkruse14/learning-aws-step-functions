67350f6233e662cb22d1896132d2911c77fe51b8
Justin <justin.kruse@flosports.tv>
2018-12-13 10:22:17 -0600

    all the code

---
+++ src/controllers/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+const Promise = require('bluebird');
+
+module.exports.hello = (input, context) => {
+    const success = input.success === 'success';
+    return Promise.resolve({ msg: 'Hello,', name: input.name, success });
+};
+
+module.exports.world = (input, context) => {
+    return Promise.resolve({ msg: 'world!', waitSeconds: 5, hello: input});
+}
+
+module.exports.fromUsername = (input, context) => {
+    const name = input.hello.name || 'Someone';
+    const msg = `${input.hello.msg} ${input.msg} ${name} greets you!`;
+    return Promise.resolve({msg});
+}
